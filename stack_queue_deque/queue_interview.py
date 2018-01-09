aa48d4e997c6a9b2fbbf8de14549493851b703cd
Oleh Dubno <olehdubno@gmail.com>
2018-01-08 20:19:55 -0500

    queue_interview

---
+++ stack_queue_deque/queue_interview.py
@@ -0,0 +1,27 @@
+# Implement a Queue
+
+class Queue:
+    """
+    LIFO - Last in First out
+    """
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        # check if the Queue is empty
+        return self.items == []
+
+    def enqueue(self, item):
+        # items are added at one end; in this case in the beginining
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        # items are removed from the other end; in this case from the end
+        return self.items.pop()
+
+    def size(self):
+        # return the size of the Queue
+        return len(self.items)
+
+if __name__ == '__main__':
+    q = Queue()
