b5b69b7b06f12383d1bae8d07c39035898dc8293
Oleh Dubno <olehdubno@gmail.com>
2018-01-14 17:40:21 -0500

    reverse_string update

--- recursion/reverse_string.py
+++ recursion/reverse_string.py
@@ -6,22 +6,40 @@ def reverse(s):
     :return:
     Recursively reverse the input string
     """
+    # Base Case
     if not s:
         return ''
+    # Recursive Case
     return reverse(s[1:]) + s[0]
 
-memo = {}
 def reverse2(s):
     """
     :param s:
     :return:
     Recursively reverse the input string
     """
+    # Base Case
+    if not s:
+        return ''
+
+    # Recursive Case
+    return s[-1] + reverse(s[:-1])
+
+memo = {}
+def reverse3(s):
+    """
+    :param s:
+    :return:
+    Recursively reverse the input string
+    Uses memoization
+    """
     if not s:
         return ''
     if s not in memo:
         memo[s] = reverse2(s[1:]) + s[0]
     return memo[s]
+
+
 
 class Memoization:
 
